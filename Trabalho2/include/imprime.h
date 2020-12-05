40a990f76304f6f37dc14e14e258b736843290af
bfsgr <bfsgr.code@protonmail.com>
2020-12-05 19:07:01 -0300

    printar estatisticas2

--- Trabalho2/include/imprime.h
+++ Trabalho2/include/imprime.h
@@ -5,4 +5,4 @@
 void imprime();
 void printa_pagina(Pagina *p, int rrn);
 void extrai_filhos(Pagina *p,int filhos[]);
-void caminha(FILE* tree, int rrn_dir, int rrn_esq);
+void caminha(FILE* tree, int rrn_dir, int rrn_esq, int *paginas, int *chaves, int *altura);
